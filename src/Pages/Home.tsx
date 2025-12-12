6ddacb6ec24a0146879599f21b37e343bc684f70
joaorema <joaopcrema@gmail.com>
2025-12-12 20:26:03 +0000

    Quick change in Home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
 
   return (
     <div className="top-div">
-      <h1 className="font-mono">Jogos</h1>
+      <h1 className="font-mono">Home</h1>
       <Button1 href="/games" title="Jogar">
       </Button1>
     </div>
