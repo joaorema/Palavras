1b05cf24484b30425b67e915c4c17b36010a7c60
joaorema <joaopcrema@gmail.com>
2026-01-19 14:40:06 +0000

    fix for incorrect display of cards on bigger screens.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,17 +17,20 @@ const GameCard: React.FC<GameProps> = ({
   return (
     <motion.button
       onClick={onClick}
-      whileHover={{ scale: 1.1, y: -4 }}
+      whileHover={{ scale: 1.05, y: -4 }}
       whileTap={{ scale: 0.95 }}
-      transition={{ type:"spring", stiffness: 300, damping: 15 }}
       className="
         flex flex-col items-center justify-between 
         bg-white/10 backdrop-blur-md 
         border border-white/20 
         rounded-2xl overflow-hidden
-        transition duration-100 ease-in-out 
         shadow-md hover:shadow-[0_0_15px_rgba(255,255,255,0.3)]
-        withd-20 height-56
+        
+        /* RESPONSIVE SIZING */
+        w-64 h-80           /* Default size (Mobile/Tablet) */
+        md:w-72 md:h-96     /* Medium screens */
+        lg:w-80 lg:h-[400px] /* Large screens - cap it here */
+        transition duration-200
       "
     >
       
